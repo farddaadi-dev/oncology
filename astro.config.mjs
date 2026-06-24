171e22e08a1e69acae6b6b95defa7d1e47e3a5f9
faddaadi-dev <farddaadi@gmail.com>
2026-06-24 18:03:23 -0400

    font fixed mobile fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,58 +1,36 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-
-// site: '[https://yourwebsite.com](https://yourwebsite.com)'
-
-fonts: [
+  fonts: [
     {
       provider: fontProviders.local(),
-      name: "Vazirmatn-Light",
-      cssVariable:"--font-vazirmatn-light",
+      name: "Vazirmatn",
+      cssVariable: "--font-vazirmatn",
       options: {
-        variants: [{
-          src: ['./src/assets/fonts/Vazirmatn-Light.woff2'],
-          weight: '300',
-          style: 'normal'
-        }]
-      }
-    },
-    {
-      provider: fontProviders.local(),
-      name: "Vazirmatn-Regular",
-      cssVariable:"--font-vl",
-      options: {
-        variants: [{
-          src: ['./src/assets/fonts/Vazirmatn-Regular.woff2'],
-          weight: '400',
-          style: 'normal'
-        }]
-      }
-    },
-    {
-      provider: fontProviders.local(),
-      name: "Vazirmatn-Medium",
-      cssVariable: "--font-vazirmatn-medium",
-      options: {
-        variants: [{
-          src: ['./src/assets/fonts/Vazirmatn-Medium.woff2'],
-          weight: '500',
-          style: 'normal'
-        }]
-      }
-    },
-    {
-      provider: fontProviders.local(),
-      name: "Vazirmatn-Bold",
-      cssVariable: "--font-vazirmatn-bold",
-      options: {
-        variants: [{
-          src: ['./src/assets/fonts/Vazirmatn-Bold.woff2'],
-          weight: '700',
-          style: 'normal'
-        }]
+        variants: [
+          {
+            src: ["./src/assets/fonts/Vazirmatn-Light.woff2"],
+            weight: "300",
+            style: "normal"
+          },
+          {
+            src: ["./src/assets/fonts/Vazirmatn-Regular.woff2"],
+            weight: "400",
+            style: "normal"
+          },
+          {
+            src: ["./src/assets/fonts/Vazirmatn-Medium.woff2"],
+            weight: "500",
+            style: "normal"
+          },
+          {
+            src: ["./src/assets/fonts/Vazirmatn-Bold.woff2"],
+            weight: "700",
+            style: "normal"
+          }
+        ]
       }
     },
     {
@@ -60,11 +38,13 @@ fonts: [
       name: "Estedad",
       cssVariable: "--font-estedad",
       options: {
-        variants: [{
-          src: ['./src/assets/fonts/Estedad-Bold.woff2'],
-          weight: '700',
-          style: 'normal'
-        }]
+        variants: [
+          {
+            src: ["./src/assets/fonts/Estedad-Bold.woff2"],
+            weight: "700",
+            style: "normal"
+          },
+        ]
       }
     }
   ]
