686e30f3b5813db4b9ef819043ed72207c570188
farddaadi-dev <farddaadi@gmail.com>
2026-07-03 07:11:53 -0400

    contact page part 1

---
+++ src/data/maps.ts
@@ -0,0 +1,20 @@
+export const maps = [
+    {
+      name: "نشان",
+      image: "/maps/neshan.webp",
+      url: "https://nshn.ir/e1_bvkV-OxMQfI",
+      icon: "/icons/neshan.svg"
+    },
+    {
+      name: "بلد",
+      image: "/maps/balad.webp",
+      url: "https://balad.ir/p/6xa1QNpXQKdCQD",
+      icon: "/icons/balad.svg"
+    },
+    {
+      name: "Google Maps",
+      image: "/maps/google.webp",
+      url: "https://maps.app.goo.gl/wW36vqp7AU6sw2Pm7",
+      icon: "/icons/google-maps.svg"
+    }
+  ];
