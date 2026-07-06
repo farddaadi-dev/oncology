cc8ddf6b4cde5329b849521355fe122d058697a7
faddaadi-dev <farddaadi@gmail.com>
2026-07-05 20:42:07 -0400

    contact part 2

--- src/data/maps.ts
+++ src/data/maps.ts
@@ -1,19 +1,19 @@
 export const maps = [
     {
       name: "نشان",
-      image: "/maps/neshan.webp",
+      image: "/images/neshan.webp",
       url: "https://nshn.ir/e1_bvkV-OxMQfI",
       icon: "/icons/neshan.svg"
     },
     {
       name: "بلد",
-      image: "/maps/balad.webp",
+      image: "/images/balad.webp",
       url: "https://balad.ir/p/6xa1QNpXQKdCQD",
       icon: "/icons/balad.svg"
     },
     {
       name: "Google Maps",
-      image: "/maps/google.webp",
+      image: "/images/google.webp",
       url: "https://maps.app.goo.gl/wW36vqp7AU6sw2Pm7",
       icon: "/icons/google-maps.svg"
     }
