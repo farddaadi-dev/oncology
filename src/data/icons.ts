d6e45c935e70858b0e04decbb21bc570465c9065
farddaadi-dev <farddaadi@gmail.com>
2026-07-16 05:12:50 -0400

    consultation3

---
+++ src/data/icons.ts
@@ -0,0 +1,9 @@
+import InfoIcon from "@/assets/icons/info.svg";
+import AlertIcon from "@/assets/icons/triangle-alert.svg";
+import ShieldIcon from "@/assets/icons/shield.svg";
+
+export const icons = {
+  info: InfoIcon.src,
+  alert: AlertIcon.src,
+  shield: ShieldIcon.src
+};
